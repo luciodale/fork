46e67192327ac00f74108c35623fd95b7eb11ff4
Lucio D'Alessandro <lda@juxt.pro>
2019-12-17 18:48:25 +0000

    Revisit validation and update API

--- src/fork/core.cljs
+++ src/fork/core.cljs
@@ -1,4 +1,4 @@
-(ns fork.fork
+(ns fork.core
   (:require
    [fork.logic :as logic]
    [reagent.core :as r]
@@ -6,7 +6,9 @@
 
 (defn fork
   [props component]
-  (let [state (r/atom {:values (:initial-values props)})]
+  (let [state (r/atom {:values (:initial-values props)})
+        form-id (or (:form-id props)
+                    (str (gensym)))]
     (r/create-class
      {:component-will-unmount
       (fn []
@@ -14,25 +16,27 @@
           (rf/dispatch [::logic/clean (:path props)])))
       :reagent-render
       (fn [props component]
-        (let [props (merge props {:state state
-                                  :validation (or (:validation props)
-                                                  (constantly nil))})
-              db @(rf/subscribe [::logic/db (:path props)])]
+        (let [db @(rf/subscribe [::logic/db (:path props)])
+              props (merge props
+                           {:state state
+                            :form-id form-id
+                            :validation
+                            (when (:validation props)
+                              (logic/handle-validation @state props))})]
           [component
            {:state state
             :path (:path props)
+            :form-id form-id
             :values (:values @state)
-            :validation (:validation props)
-            :initial-values (:initial-values props)
-            :errors (logic/errors (:errors @state)
-                                    (:external-errors db))
+            :errors (:validation props)
             :touched (:touched @state)
             :submitting? (:submitting? db)
-            :submit-count (:submit-count db)
+            :submit-count (:submit-count @state)
             :set-values #(logic/set-values % props)
             :disable (fn [& ks] (logic/local-disable props ks))
             :enable (fn [& ks] (logic/local-enable props ks))
-            :disabled? #(logic/disabled? (:disabled? @state) (:disabled? db) %)
+            :disabled? #(logic/disabled? (:disabled? @state)
+                                         (:disabled? db) %)
             :handle-change #(logic/handle-change % props)
             :handle-blur #(logic/handle-blur % props)
             :handle-submit #(logic/handle-submit % props)}]))})))
@@ -65,41 +69,10 @@
   [db path errors-map]
   (logic/set-external-errors db path errors-map))
 
-;; ---- Input array to handle dynamically generated forms ---- ;;
-
-(defn input-array
-  [props {:keys [name component args]}]
-  [component (merge
-              (dissoc props :handle-change :handle-blur)
-              {:array-key name
-               :handle-change
-               (fn [evt idx]
-                 (logic/handle-on-change-input-array
-                  evt props name idx))
-               :handle-blur
-               (fn [evt idx]
-                 (logic/handle-on-blur-input-array
-                  evt props name idx))
-               :input-array-errors
-               (fn [idx input-key err-ks]
-                 (logic/input-array-errors
-                  (merge props {:array-key name})
-                  idx input-key err-ks))
-               :add
-               (fn [evt]
-                 (.preventDefault evt)
-                 (logic/add-to-input-array
-                  props name))
-               :delete
-               (fn [evt idx & [err-ks]]
-                 (.preventDefault evt)
-                 (logic/delete-from-input-array
-                  props name idx err-ks))}) args])
-
 ;; ---- Input templates using Bulma CSS Framework ---- ;;
 
 (defn input
-  [{:keys [values errors touched handle-change handle-blur]}
+  [{:keys [values touched handle-change handle-blur disabled?]}
    {:keys [label placeholder name type class]}]
   [:div.field {:class class}
    [:label.label label]
@@ -109,15 +82,12 @@
       :placeholder placeholder
       :type type
       :value (values name "")
+      :disabled (disabled? name)
       :on-change handle-change
-      :on-blur handle-blur}]]
-   (when (get touched name)
-     (for [[k msg] (get errors name)]
-       ^{:key k}
-       [:p.help msg]))])
+      :on-blur handle-blur}]]])
 
 (defn textarea
-  [{:keys [values errors touched handle-change handle-blur]}
+  [{:keys [values touched handle-change handle-blur disabled?]}
    {:keys [label placeholder name class]}]
   [:div.field {:class class}
    [:label.label label]
@@ -126,15 +96,12 @@
      {:name name
       :value (values name "")
       :placeholder placeholder
+      :disabled (disabled? name)
       :on-change handle-change
-      :on-blur handle-blur}]]
-   (when (get touched name)
-     (for [[k msg] (get errors name)]
-       ^{:key k}
-       [:p.help msg]))])
+      :on-blur handle-blur}]]])
 
 (defn checkbox
-  [{:keys [values errors touched handle-change handle-blur]}
+  [{:keys [values touched handle-change handle-blur disabled?]}
    {:keys [name class text]}]
   [:div.field {:class class}
    [:div.control
@@ -143,18 +110,14 @@
       {:name name
        :type "checkbox"
        :checked (values name false)
+       :disabled (disabled? name)
        :on-change handle-change
        :on-blur handle-blur}]
-     " " text]]
-   (when (get touched name)
-     (for [[k msg] (get errors name)]
-       ^{:key k}
-       [:p.help msg]))])
+     " " text]]])
 
 (defn- dropdown-inner
   [active? ref
-   {:keys [values errors handle-change
-           handle-blur]}
+   {:keys [values handle-change handle-blur]}
    {:keys [label name options class]}]
   [:div.field
    [:label.label label]
@@ -184,10 +147,7 @@
           {:name name
            :value value
            :on-click handle-change}
-          option]])]]]
-   (for [[k msg] (get errors name)]
-     ^{:key k}
-     [:p.help msg])])
+          option]])]]]])
 
 (defn pretty-dropdown
   [props opts]
@@ -221,8 +181,7 @@
                       {:options options})])})))
 
 (defn dropdown
-  [{:keys [values errors touched handle-change
-           handle-blur]}
+  [{:keys [values touched handle-change handle-blur disabled?]}
    {:keys [label name options class]}]
   [:div.field
    {:class class}
@@ -233,13 +192,10 @@
       {:name name
        :value (values name)
        :on-change handle-change
-       :on-blur handle-blur}
+       :on-blur handle-blur
+       :disabled (disabled? name)}
       (for [[value option] options]
         ^{:key value}
         [:option
          {:value value}
-         option])]]]
-   (when (get touched name)
-     (for [[k msg] (get errors name)]
-       ^{:key k}
-       [:p.help msg]))])
+         option])]]]])
