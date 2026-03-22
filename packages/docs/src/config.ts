0eee02324bee715ae2112c4fdca9b7f34749de48
Lucio D'Alessandro <lda@juxt.pro>
2026-03-22 11:46:30 +0100

    use logo illustration

--- packages/docs/src/config.ts
+++ packages/docs/src/config.ts
@@ -55,6 +55,6 @@ export const siteConfig: SiteConfig = {
 	parentSite: {
 		href: "https://koolcodez.com/projects",
 		label: "koolcodez",
-		logoSrc: "/kool-codez-logo.svg",
+		logoSrc: "/kool-codez-illustration.svg",
 	},
 };
